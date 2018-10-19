186fa5dbfc4c527c371cedbbb03df4257b981aea
Julio Berina <julioberina@gmail.com>
2018-10-19 13:30:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# CS 2640 Project 2
+
+This is the one file for Project 2 that calculates N amount of 
+Fibonacci numbers with N being based on user input.
---
+++ fibonacci.asm
@@ -0,0 +1,18 @@
+# Who:  Julio Berina
+# What: fibonacci.asm
+# Why:  Calculate the Fibonacci number for the nth term
+# When: 10/19/18
+# How:  Calculate and store each term in an array
+
+.data
+
+.text
+.globl main
+
+
+main:  # program entry
+
+
+
+li $v0, 10
+syscall
